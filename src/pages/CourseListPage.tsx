77d984e9f91e78e0c55f1e9e41f706476cd3eb14
bit1creativee <ivan.hrytsenko@nbcuni.com>
2023-03-18 12:21:39 +0200

    initial set-up

---
+++ src/pages/CourseListPage.tsx
@@ -0,0 +1,9 @@
+const CourseListPage = () => {
+    return (
+        <div className='min-h-screen flex justify-center items-center'>
+            <h1 className='text-3xl font-bold text-blue-600'>CourseListPage</h1>
+        </div>
+    );
+};
+
+export default CourseListPage;
---
+++ src/router.tsx
@@ -0,0 +1,16 @@
+import { createBrowserRouter } from 'react-router-dom';
+import CourseListPage from './pages/CourseListPage';
+import CourseViewPage from './pages/CourseViewPage';
+import ErrorPage from './pages/ErrorPage';
+
+export default createBrowserRouter([
+    {
+        path: '/',
+        element: <CourseListPage />,
+        errorElement: <ErrorPage />
+    },
+    {
+        path: '/course',
+        element: <CourseViewPage />
+    }
+]);
